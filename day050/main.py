05eacfd16cb4c1b3f84495c928e5fef833245c1a
Christopher Boardman <89698445+GameMill@users.noreply.github.com>
2021-10-22 23:21:08 +0100

    update

--- day050/main.py
+++ day050/main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.common.by import By
-from datetime import datetime
+from selenium.webdriver.common.keys import Keys
 import json
 import os
 import time
@@ -19,7 +19,7 @@ driver.get("https://tinder.com/")
 driver.find_element(By.XPATH,'//*[@id="q-1565082725"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a').click()
 time.sleep(1)
 driver.find_element(By.XPATH,'//*[@id="q-1343742289"]/div/div/div[1]/div/div[3]/span/div[2]/button').click()
-
+time.sleep(1)
 Tinder_Window = driver.window_handles[0]
 Facebook_Window = driver.window_handles[1]
 driver.switch_to.window(Facebook_Window)
@@ -27,10 +27,16 @@ driver.switch_to.window(Facebook_Window)
 print(driver.title)
 time.sleep(1)
 
-driver.find_element(By.ID,"u_0_8_Pj").click()
-
 driver.find_element(By.ID,"email").send_keys(facebook_data["email"])
 
 driver.find_element(By.ID,"pass").send_keys(facebook_data["password"])
-#driver.find_element(By.NAME,"login").click()
+driver.find_element(By.ID,"pass").send_keys(Keys.ENTER)
+#driver.find_element(By.XPATH,'//*[@id="u_0_0_RL"]').click()
+
+driver.switch_to.window(Tinder_Window)
+print(driver.title)
+time.sleep(5)
+driver.find_element(By.XPATH,'//*[@id="q-1343742289"]/div/div/div/div/div[3]/button[1]').click()
+driver.find_element(By.XPATH,'//*[@id="q-1343742289"]/div/div/div/div/div[3]/button[2]').click()
+driver.find_element(By.XPATH,'//*[@id="q-1565082725"]/div/div[2]/div/div/div[1]/button').click()
 input()
